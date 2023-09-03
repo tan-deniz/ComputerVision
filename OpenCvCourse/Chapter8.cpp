86eca7b4fb8ce5d885540ebcc7961416de68f3db
Tan <Tan@DESKTOP-L211NS6>
2023-09-03 14:15:32 +0300

    Face_Reconition2

--- OpenCvCourse/Chapter8.cpp
+++ OpenCvCourse/Chapter8.cpp
@@ -16,12 +16,16 @@ void main() {
     Mat img = imread(path);
 
     CascadeClassifier faceCascade;
-    faceCascade.load("Resources/haarcascadee_frontalface_default.xml");
+
+    faceCascade.load("Resources/haarcascade_frontalface_default.xml");
     if (faceCascade.empty()) {cout << "XML file not loaded" << endl;}
 
     vector<Rect> faces;
-    faceCascade.detectMultiScale(img,faces,1.1,10)
-
+    faceCascade.detectMultiScale(img, faces, 1.1, 10);
+    for (int i = 0; i<faces.size(); i++)
+    {
+        rectangle(img, faces[i].tl(), faces[i].br(), Scalar(255, 0, 255), 3);
+    }
 
     imshow("Image", img);
     waitKey(0);
